5a39952a439f2b9df614f41a251c32950b151939
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-11 20:29:10 +0530

    Interface and implementation 2.5

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -1,8 +1,38 @@
-#include <iostream>
-#include "console.h"
+/* File direction.h
+ * The interface
+ * #ifndef _direction_h
+ * #define _direction_h
+ *
+ * #include <string>
+ *
+ * enum Direction { NORTH, EAST, SOUTH, WEST };
+ *
+ * Direction leftFrom(Direction dir);
+ *
+ * Direction rightFrom(Direction dir);
+ *
+ * std::string directionToString(Direction dir);
+ *
+ */
+
+#include <string>
+#include <direction.h>
 using namespace std;
 
-int main() {
-    cout << "Hello";
-    return 0;
+Direction leftFrom(Direction dir) {
+    return Direction((dir + 3) % 4);
+}
+
+Direction rightFrom(Direction dir) {
+    return Direction((dir + 1) % 4);
+}
+
+string directionToString(Direction dir) {
+    switch(dir) {
+        case NORTH: return "NORTH";
+    case EAST: return "EAST";
+    case SOUTH: return "SOUTH";
+    case WEST: return "WEST";
+    default: return "??";
+    }
 }
