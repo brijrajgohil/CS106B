3fc23bff9b228db741ca85a2cf68d0ae470a4ea6
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-11 18:00:40 +0530

    Practicing

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -1,31 +1,8 @@
-#include<iostream>
-#include"console.h"
-
+#include <iostream>
+#include "console.h"
 using namespace std;
 
-/* Function prototypes */
-int combination(int n, int k);
-int fact(int n);
-
-/* Main program */
 int main() {
-    int n, k;
-    cout << "Enter the number of objects(n): ";
-    cin >> n;
-    cout << "Enter the number to be chosen(k): ";
-    cin >> k;
-    cout << "C(n, k) = " << combination(n, k) << endl;
+    cout << "Hello";
     return 0;
-}
-
-int combination(int n, int k) {
-    return fact(n) / (fact(k) * fact(n - k));
-}
-
-int fact(int n) {
-    int result = 1;
-    for(int i = 1; i <= n; i++) {
-        result *= i;
-    }
-    return result;
 }
