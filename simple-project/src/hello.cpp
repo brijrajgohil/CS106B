efeec9541d06df2e067a3ffd230a3d0e25aaa477
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-12 21:52:47 +0530

    2.6

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -3,30 +3,22 @@
 
 using namespace std;
 
-//Prototype
-bool isPerfect(int num);
+bool isPrime(int num);
 
 int main() {
-    for(int i = 1; i <= 9999; i++) {
-        if(isPerfect(i)) {
+    for(int i = 2; i <= 100; i++) {
+        if(isPrime(i)) {
             cout << i << endl;
         }
     }
     return 0;
 }
 
-bool isPerfect(int num) {
-
-    int total = 0;
-    for(int i = 1; i < num; i++) {
+bool isPrime(int num) {
+    for(int i = 2; i < num; i++) {
         if(num % i == 0) {
-            total += i;
-
+            return false;
         }
     }
-
-    if(total == num) {
-        return true;
-    }
-    return false;
+    return true;
 }
