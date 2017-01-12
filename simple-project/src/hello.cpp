5a71bf060a07a5b0504cfff001228f82695c14e7
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-12 17:11:58 +0530

    Random.cpp

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -1,69 +1,44 @@
-/* File: random.h
- *
- * #ifndef _random_h
- * #define _random_h
- *
- * int randomInteger(int low, int high);
- *
- * double randomReal(double low, double high);
- *
- * bool randomChance(double p);
- *
- * void setRandomSeed(int seed);
- *
- * #endif
- *
+/* File random.cpp
+ * Avoid renaming because it's a tedius process
  */
 
-#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
 #include "random.h"
-#include "console.h"
+
 using namespace std;
 
-bool tryToMakePoint(int point);
-int rollTwoDice();
+void initRandomSeed();
 
-int main() {
-    cout << "This program plays a game of craps. " << endl;
-    int point = rollTwoDice();
+int randomInteger(int low, int high) {
+    initRandomSeed();
+    double d = rand() / (double(RAND_MAX) + 1);
+    double s = d * (double(high) - low + 1);
+    return int(floor(low + s));
+}
 
-    switch(point) {
-        case 7: case 11:
-            cout << "That's a natural. You win." << endl;
-            break;
-        case 2: case 3: case 12:
-            cout << "That's craps. You lose." << endl;
-            break;
-        default:
-            cout << "Your point is " << point << "." << endl;
-            if (tryToMakePoint(point)) {
-                cout << "You made your point. You win." << endl;
-            } else {
-                cout << "You rolled a seven. You lose." << endl;
-            }
+double randomReal(double low, double high) {
+    initRandomSeed();
+    double d = rand() / (double(RAND_MAX) + 1);
+    double s = d * (high - low);
+    return low + s;
+}
+
+bool randomChoice(double p) {
+    initRandomSeed();
+    return randomReal(0, 1) < p;
+}
+
+void setRandomSeed(int seed) {
+    initRandomSeed();
+    srand(seed);
+}
+
+void initRandomSeed() {
+    static bool initialized = false;
+    if(!initialized) {
+        srand(int(time(NULL)));
+        initialized = true;
     }
-    return 0;
 }
-
-bool tryToMakePoint(int point) {
-    while(true) {
-        int total = rollTwoDice();
-        if (total == point) return true;
-        if (total == 7) return false;
-    }
-}
-
-int rollTwoDice() {
-    cout << "Rolling the dice ..." << endl;
-    int d1 = randomInteger(1, 6);
-    int d2 = randomInteger(1, 6);
-    int total = d1 + d2;
-    cout << "You rolled " << d1 << " and " << d2
-         << " - that's " << total << endl;
-    return total;
-}
-
-
-
-
-
