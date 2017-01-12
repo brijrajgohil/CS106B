247dc7d2f6e814eb41e55d4f5d0bbc9fe04eb0db
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-12 17:26:13 +0530

    Graphics program

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -1,44 +1,14 @@
-/* File random.cpp
- * Avoid renaming because it's a tedius process
- */
-
-#include <cstdlib>
-#include <cmath>
-#include <ctime>
-#include "random.h"
+#include "gobjects.h"
+#include <iostream>
 
 using namespace std;
 
-void initRandomSeed();
-
-int randomInteger(int low, int high) {
-    initRandomSeed();
-    double d = rand() / (double(RAND_MAX) + 1);
-    double s = d * (double(high) - low + 1);
-    return int(floor(low + s));
-}
-
-double randomReal(double low, double high) {
-    initRandomSeed();
-    double d = rand() / (double(RAND_MAX) + 1);
-    double s = d * (high - low);
-    return low + s;
-}
-
-bool randomChoice(double p) {
-    initRandomSeed();
-    return randomReal(0, 1) < p;
-}
-
-void setRandomSeed(int seed) {
-    initRandomSeed();
-    srand(seed);
-}
-
-void initRandomSeed() {
-    static bool initialized = false;
-    if(!initialized) {
-        srand(int(time(NULL)));
-        initialized = true;
-    }
+int main() {
+    initGraphics();
+    setColor("BLUE");
+    fillRect(50, 50, 300, 150);
+    setFont("SansSerif-Bold-48");
+    setColor("WHITE");
+    drawString("hello, world", 60, 140);
+    return 0;
 }
