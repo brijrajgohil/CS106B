026dca0018b1388bdd164ab51a8960f93109b276
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-12 22:34:43 +0530

    2.7

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -3,22 +3,31 @@
 
 using namespace std;
 
-bool isPrime(int num);
+void calculateEaster(int year);
 
 int main() {
-    for(int i = 2; i <= 100; i++) {
-        if(isPrime(i)) {
-            cout << i << endl;
-        }
-    }
+    calculateEaster(1744);
+    calculateEaster(1815);
     return 0;
 }
 
-bool isPrime(int num) {
-    for(int i = 2; i < num; i++) {
-        if(num % i == 0) {
-            return false;
-        }
+void calculateEaster(int year) {
+    int a = year % 19;
+    int b = year % 4;
+    int c = year % 7;
+    int d = ((19 * a) + 23) % 30;
+    int e;
+    if(year >= 1700 && year <= 1799) {
+        e = ((2 * b) + (4 * c) + (6 * d) + 3) % 7;
+    } else if(year >= 1800 && year <= 1899) {
+        e = ((2 * b) + (4 * c) + (6 * d) + 4) % 7;
     }
-    return true;
+
+    if((d + e) > 9) {
+        cout << "April " << d + e - 9 << endl;
+    }
+    else {
+        cout << "March " << 22 + d + e << endl;
+    }
+
 }
