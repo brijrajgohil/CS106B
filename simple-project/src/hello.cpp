f485cd77161e40c56380d12745d82f59160245dd
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-12 21:47:26 +0530

    2.5

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -1,1 +1,32 @@
-#include<iostream>
+#include <iostream>
+#include "console.h"
+
+using namespace std;
+
+//Prototype
+bool isPerfect(int num);
+
+int main() {
+    for(int i = 1; i <= 9999; i++) {
+        if(isPerfect(i)) {
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
+
+bool isPerfect(int num) {
+
+    int total = 0;
+    for(int i = 1; i < num; i++) {
+        if(num % i == 0) {
+            total += i;
+
+        }
+    }
+
+    if(total == num) {
+        return true;
+    }
+    return false;
+}
