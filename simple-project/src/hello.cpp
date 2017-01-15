bb4136b16cffdbc4b936e34e97f4f8b1a332182f
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-15 12:48:20 +0530

    heads, heads, heads

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -4,24 +4,20 @@
 
 using namespace std;
 
-void randomDecay(int &a);
-
 int main() {
-    int a = 10000, years = 1;
-    cout << "There are "<< a <<" atoms initially." << endl;
-    while(a > 0) {
-        randomDecay(a);
-        cout << " There are " << a << " atoms at the end of year " << years << endl;
-        years++;
+    int hCount = 0;
+    int total = 0;
+    while(hCount != 3) {
+        if(randomChance(0.5)) {
+            hCount++;
+            cout << "heads" << endl;
+        } else {
+            hCount = 0;
+            cout << "tails" << endl;
+        }
+        total++;
     }
-    return 0;
-}
 
-void randomDecay(int &a) {
-    for(int i = 1; i <= a; i++) {
-        if(randomChance(0.5))
-            continue;
-        else
-            a--;
-    }
+    cout << "The total number of iterations " << total;
+    return 0;
 }
