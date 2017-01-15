c54ca8a370dcda0f61a635276316e0f3cec43235
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-15 14:42:14 +0530

    Playing with graphics

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include "gwindow.h"
+#include "gobjects.h"
+
+using namespace std;
+
+//void drawArc(GWindow &gw, GArc *arc, string color);
 
 int main() {
     GWindow gw;
-    gw.setColor("CYAN");
-    gw.fillRect(0.0, 0.0, gw.getWidth(), gw.getHeight());
+    double width = gw.getWidth();
+    double height = gw.getHeight();
+    //gw.setColor("CYAN");
+    //gw.fillRect(0.0, 0.0, width, height);
+    GArc *arc = new GArc(width, (height / 2), 0, 180);
+    arc->setFilled(true);
+    arc->setFillColor("MAGENTA");
+    gw.add(arc, width / 2, 0);
+
+//    drawArc(arc, "MAGENTA");
+//    drawArc(arc, "BLUE");
+//    drawArc(arc, "GREEN");
+//    drawArc(arc, "YELLOW");
+//    drawArc(arc, "ORANGE");
+//    drawArc(arc, "RED");
     return 0;
 }
+
+//void drawArc(GWindow &gw, GArc *arc, string color) {
+
+//}
+
+
