d313b6ce01bb423c5927d5d579b621ffcae4699f
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-16 17:01:43 +0530

    Rainbow using gwindow.h

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -1,33 +1,56 @@
-#include <iostream>
 #include "gwindow.h"
-#include "gobjects.h"
+#include <iostream>
 
 using namespace std;
 
-//void drawArc(GWindow &gw, GArc *arc, string color);
+double const RAINBOW_THICKNESS = 15.0;
+double const RAINBOW_SIZE = 200;
+
+void setBackgroundColor(GWindow &gw);
+void drawRainbow(GWindow &gw);
+string colorToString(int color);
 
 int main() {
     GWindow gw;
-    double width = gw.getWidth();
-    double height = gw.getHeight();
-    //gw.setColor("CYAN");
-    //gw.fillRect(0.0, 0.0, width, height);
-    GArc *arc = new GArc(width, (height / 2), 0, 180);
-    arc->setFilled(true);
-    arc->setFillColor("MAGENTA");
-    gw.add(arc, width / 2, 0);
-
-//    drawArc(arc, "MAGENTA");
-//    drawArc(arc, "BLUE");
-//    drawArc(arc, "GREEN");
-//    drawArc(arc, "YELLOW");
-//    drawArc(arc, "ORANGE");
-//    drawArc(arc, "RED");
+    setBackgroundColor(gw);
+    drawRainbow(gw);
     return 0;
 }
 
-//void drawArc(GWindow &gw, GArc *arc, string color) {
+void setBackgroundColor(GWindow &gw) {
+    double width = gw.getWidth();
+    double height = gw.getHeight();
+    gw.setColor("CYAN");
+    gw.fillRect(0, 0, width, height);
+}
 
-//}
+void drawRainbow(GWindow &gw) {
+    for(int i = 1; i < 8; i++) {
+        double width = (gw.getWidth() + (RAINBOW_SIZE * 2) - (RAINBOW_THICKNESS * i) * 2);
+        double y = RAINBOW_THICKNESS * i;
+        double x = (RAINBOW_THICKNESS * i) - RAINBOW_SIZE;
+        gw.setColor(colorToString(i));
+        gw.fillOval(x, y, width, width);
+    }
+}
 
-
+string colorToString(int color) {
+    switch (color) {
+            case 1:
+                return "RED";
+            case 2:
+                return "ORANGE";
+            case 3:
+                return "YELLOW";
+            case 4:
+                return "GREEN";
+            case 5:
+                return "BLUE";
+            case 6:
+                return "MAGENTA";
+            case 7:
+                return "CYAN";
+            default:
+                return "BLACK";
+    }
+}
