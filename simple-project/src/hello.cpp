81f220dac77167800cf706a09eeeb2228bc27d45
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-18 20:14:25 +0530

    Revert "2.6"
    This reverts commit 262cea7ea36d311d86dc74b7eaec50223f72a98a.

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
 #include <string>
-#include "console.h"
 #include <cctype>
+#include "console.h"
 
 using namespace std;
 
-string acronym(string str);
-
 int main() {
-    string str;
-    cout << "Enter the string: ";
-    getline(cin, str);
-    cout << acronym(str) << endl;
+    string words;
+    int score = 0;
+    cout << "Enter the words: ";
+    getline(cin, words);
+    for (int i = 0; i < words.length(); i++) {
+        if (isupper(words[i])) {
+            switch (words[i]) {
+            case 'A': case 'E': case 'I': case 'L': case 'N':
+            case 'O': case 'R': case 'S': case 'T': case 'U':
+                score += 1;
+                break;
+            case 'D': case 'G':
+                score += 2;
+                break;
+            case 'B': case 'C': case 'M': case 'P':
+                score += 3;
+                break;
+            case 'F': case 'H': case 'V': case 'W': case 'Y':
+                score += 4;
+                break;
+            case 'J': case 'X':
+                score += 8;
+                break;
+            case 'Q': case 'Z':
+                score += 10;
+                break;
+            default:
+                break;
+            }
+        }
+
+    }
+
+    cout <<"You scored: " << score << endl;
     return 0;
 }
 
-string acronym(string str) {
-    string temp;
-    int t = 0;
-    if(isalpha(str[0])){
-        temp += str[0];
-    }
-    for(int i = 0; i < str.length(); i++) {
-        if(!isalpha(str[i])) {
-            t = -1;
-            continue;
-        } else if(t == -1) {
-            temp += str[i];
-            t = 1;
-        }
-    }
-    return temp;
-}
