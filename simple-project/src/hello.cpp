7943e32c1eb9b1880d079077c70498f536465cc7
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-18 17:33:27 +0530

    3.4

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -1,42 +1,23 @@
 #include <iostream>
-#include "console.h"
 #include <string>
+#include <cctype>
+#include "console.h"
 
 using namespace std;
 
-string mySub(string str, int pos, int n);
-string mySub(string str, int pos);
+string capitalize(string str);
 
 int main() {
-    cout << mySub("Hello world", 6) << endl; //returns "world"
-    cout << mySub("Hakuna matata", 9, 20) << endl; //returns "tata"
-    cout << mySub("Happy coding", 54) << endl; //returns error
+    cout << capitalize("HELLO"); //returns "Hello"
+    cout << capitalize("hello"); //returns "Hello"
     return 0;
 }
 
-string mySub(string str, int pos, int n) {
+string capitalize(string str) {
     string temp;
-    if(pos >= str.length()) {
-        return "Error";
-    }
-    int length = str.length();
-    if(n >= length || ((pos + n) >= str.length())) {
-        n = str.length() - 1;
-    }
-    for(int i = pos; i <= n; i++) {
-        temp += str[i];
-    }
-    return temp;
-}
-
-string mySub(string str, int pos) {
-    string temp;
-    if(pos >= str.length()) {
-        return "Error";
-    }
-    int length = str.length();
-    for(int i = pos; i < length; i++) {
-        temp += str[i];
+    temp += toupper(str[0]);
+    for(int i = 1; i < str.length(); i++) {
+        temp += tolower(str[i]);
     }
     return temp;
 }
