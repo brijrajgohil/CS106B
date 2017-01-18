37a1df840abc6b833fa4c0c0610c447b9c1b3f48
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-01-18 17:45:16 +0530

    3.5

--- simple-project/src/hello.cpp
+++ simple-project/src/hello.cpp
@@ -5,19 +5,41 @@
 
 using namespace std;
 
-string capitalize(string str);
-
 int main() {
-    cout << capitalize("HELLO"); //returns "Hello"
-    cout << capitalize("hello"); //returns "Hello"
+    string words;
+    int score = 0;
+    cout << "Enter the words: ";
+    getline(cin, words);
+    for (int i = 0; i < words.length(); i++) {
+        if (isupper(words[i])) {
+            switch (words[i]) {
+            case 'A': case 'E': case 'I': case 'L': case 'N':
+            case 'O': case 'R': case 'S': case 'T': case 'U':
+                score += 1;
+                break;
+            case 'D': case 'G':
+                score += 2;
+                break;
+            case 'B': case 'C': case 'M': case 'P':
+                score += 3;
+                break;
+            case 'F': case 'H': case 'V': case 'W': case 'Y':
+                score += 4;
+                break;
+            case 'J': case 'X':
+                score += 8;
+                break;
+            case 'Q': case 'Z':
+                score += 10;
+                break;
+            default:
+                break;
+            }
+        }
+
+    }
+
+    cout <<"You scored: " << score << endl;
     return 0;
 }
 
-string capitalize(string str) {
-    string temp;
-    temp += toupper(str[0]);
-    for(int i = 1; i < str.length(); i++) {
-        temp += tolower(str[i]);
-    }
-    return temp;
-}
